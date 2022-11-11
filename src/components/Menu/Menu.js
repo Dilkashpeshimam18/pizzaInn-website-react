b254f6c968eec0cc22f435119744bd12ee455b80
Dilkash <dilkashpeshimam@gmail.com>
2022-11-11 14:38:26 +0530

    change in duration of menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -8,7 +8,7 @@ const Menu = () => {
             initial={{ y: 10, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: -10, opacity: 0, transition: { duration: 0.1 } }}
-            transition={{ duration: 0.8 }}>
+            transition={{ duration: 0.5 }}>
             <div className='menu__body'>
                 <FoodItem />
             </div>
